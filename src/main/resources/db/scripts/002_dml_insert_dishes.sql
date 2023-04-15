c874db3892fe9a8e86592e885600ce37dbcdabe0
denfort50 <denfort50@yandex.ru>
2023-04-15 07:39:59 +0300

    Liquibase added

---
+++ src/main/resources/db/scripts/002_dml_insert_dishes.sql
@@ -0,0 +1,3 @@
+INSERT INTO dishes (name, description) VALUES ('Haukartl', 'Shark dish');
+INSERT INTO dishes (name, description) VALUES ('Ratatouille', 'Vegetable-mix dish');
+INSERT INTO dishes (name, description) VALUES ('Pasta', 'Traditional italian dish');
